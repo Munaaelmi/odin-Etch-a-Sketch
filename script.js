154c25892daba89a8581e9bb6e322d74aa5fa902
Munaaelmi <munaaelmi@outlook.com>
2025-03-24 11:15:15 +0000

    users can now select the size grid they want and it changes colour when you hover

--- script.js
+++ script.js
@@ -7,53 +7,54 @@ const divs = () => {
   container.appendChild(div);
 };
 
-// const containerTwo = () => {
-//   const container2 = document.createElement("div");
-//   container.classList.add("container");
-//   const div = document.createElement("div");
-//   div.classList.add("grid");
-//   container2.appendChild(div);
-//   document.body.append(container2);
-//   container.remove();
-// };
-
 for (let i = 0; i < 256; i++) {
   divs();
 }
-
-gridSizeBtn.addEventListener("click", () => {
-  let userResponse = prompt(
-    "How many number of squares would you like for your grid?"
-  );
-  let gridSize = userResponse;
-
-  const divs = () => {
-    const container2 = document.createElement("div");
-    const div = document.createElement("div");
-    container2.classList.add("container");
-    div.classList.add("grid");
-
-    console.log(container2, div);
-    // container2.classList.add("container");
-    // div.classList.add("grid");
-    // container2.appendChild(div);
-    document.body.append(container2);
-    // container.remove();
-
-    container2.appendChild(div);
-  };
-
-  for (let i = 0; i < gridSize; i++) {
-    divs();
-  }
-});
 
 container.addEventListener("mouseover", (e) => {
   if (e.target.tagName === "DIV") {
     let randomColourOne = Math.floor(Math.random() * 255);
     let randomColourTwo = Math.floor(Math.random() * 255);
     let randomColourThree = Math.floor(Math.random() * 255);
-
     e.target.style.backgroundColor = `rgb(${randomColourOne}, ${randomColourTwo}, ${randomColourThree})`;
+  }
+});
+
+gridSizeBtn.addEventListener("click", () => {
+  let userResponse = prompt(
+    "How many number of squares would you like for your grid between 1 -100?"
+  );
+  let gridSize = userResponse;
+
+  if (userResponse > 1 && userResponse <= 100) {
+    if (userResponse) {
+      let newContainer = document.createElement("section");
+      const divs = () => {
+        const div = document.createElement("div");
+        div.classList.add("grid");
+        newContainer.classList.add("container");
+        newContainer.appendChild(div);
+        document.body.append(newContainer);
+        container.remove();
+      };
+      for (let i = 0; i < gridSize * gridSize; i++) {
+        divs();
+      }
+
+      newContainer.addEventListener("mouseover", (e) => {
+        if (e.target.tagName === "DIV") {
+          let randomColourOne = Math.floor(Math.random() * 255);
+          let randomColourTwo = Math.floor(Math.random() * 255);
+          let randomColourThree = Math.floor(Math.random() * 255);
+          e.target.style.backgroundColor = `rgb(${randomColourOne}, ${randomColourTwo}, ${randomColourThree})`;
+        }
+      });
+
+      let section = newContainer.previousElementSibling.tagName === "SECTION";
+
+      if (section) {
+        newContainer.previousElementSibling.remove();
+      }
+    }
   }
 });
